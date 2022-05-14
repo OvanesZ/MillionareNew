0d3586bacda5fa43861ec06684122d01e56483f2
Ovanes <o-zakharyan@mail.ru>
2022-05-14 23:22:09 +0300

    add ScoreViewController

---
+++ MillionareNew/MillionareNew/View/ScoreViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ScoreViewController.swift
+//  MillionareNew
+//
+//  Created by Ованес Захарян on 14.05.2022.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+
+
+extension ScoreViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Game.shared.records.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecordCell", for: indexPath)
+        let record = Game.shared.records[indexPath.row]
+        cell.textLabel?.text = record.name
+        cell.detailTextLabel?.text = "\(record.score)"
+        
+        return cell
+        
+    }
+    
+    
+}
