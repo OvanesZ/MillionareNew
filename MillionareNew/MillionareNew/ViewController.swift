cf4a408d3277acb4b8f9e01d892740032b23855e
Ovanes <o-zakharyan@mail.ru>
2022-05-14 15:00:01 +0300

    create new project MillionareNew

---
+++ MillionareNew/MillionareNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionareNew
+//
+//  Created by Ованес Захарян on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
